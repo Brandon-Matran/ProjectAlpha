c02db30988e704e0d2ee3cf9b453807bbc4f48c5
Brandon Matran <b_matran@comcast.net>
2022-10-24 18:05:37 -0700

    Feature 13 complete

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
+from tasks.models import Task
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -11,3 +12,11 @@ def project_list(request):
         "project_object": project,
     }
     return render(request, "projects/list.html", context)
+
+@login_required
+def show_project(request, id):
+    details = get_object_or_404(Project, id=id)
+    context = {
+        "detail_object": details,
+    }
+    return render(request, "projects/details.html", context)
